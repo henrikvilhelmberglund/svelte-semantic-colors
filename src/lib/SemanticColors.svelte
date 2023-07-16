9837eaeaf7065059c3d5d03af64de243208e78e5
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-07-16 13:26:36 +0200

    first commit

---
+++ src/lib/SemanticColors.svelte
@@ -0,0 +1,56 @@
+<script>
+</script>
+
+<style>
+	:global(header) {
+		background-color: #ff5722 !important;
+	}
+	:global(nav) {
+		background-color: #2169f3 !important;
+	}
+	:global(main) {
+		background-color: #4caf50 !important;
+	}
+	:global(section) {
+		background-color: #ff9800 !important;
+	}
+	:global(article) {
+		background-color: #9c27b0 !important;
+	}
+	:global(aside) {
+		background-color: #607d8b !important;
+	}
+	:global(footer) {
+		background-color: #795548 !important;
+	}
+	:global(figure) {
+		background-color: #ffc107 !important;
+	}
+	:global(div) {
+		background-color: cornflowerblue !important;
+	}
+	:global(p) {
+		background-color: #333333 !important;
+	}
+	:global(a) {
+		background-color: #c196c3 !important;
+	}
+	:global(h1) {
+		background-color: #f06493 !important;
+	}
+	:global(h2) {
+		background-color: #ee4c83 !important;
+	}
+	:global(h3) {
+		background-color: #eb3573 !important;
+	}
+	:global(h4) {
+		background-color: #e91e63 !important;
+	}
+	:global(h5) {
+		background-color: #d81558 !important;
+	}
+	:global(h6) {
+		background-color: #c1134e !important;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+	import SemanticColors from '$lib/SemanticColors.svelte';
+</script>
+
+<h1>Welcome to your library project</h1>
+<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<SemanticColors />
+
+<header>This is a header</header>
+
+<nav>This is a nav</nav>
+
+<div>This is a div</div>
+
+<main>This is main</main>
+
+<article>This is an article</article>
+
+<section>This is section</section>
+
+<aside>This is an aside</aside>
+
+<footer>This is a footer</footer>
+
+<figure>This is a figure</figure>
+
+<h1>H1</h1>
+<h2>H2</h2>
+<h3>H3</h3>
+<h4>H4</h4>
+<h5>H5</h5>
+<h6>H6</h6>
+<p>paragraph</p>
+<a href="">link</a>
